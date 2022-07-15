42fdbb2f4f3581cb332f343231669260efbbf118
Joydeep Biswas <joydeepb@cs.utexas.edu>
2022-07-15 10:19:07 -0500

    Iterative solver using CUDA
    * Added structs to operate on sparse matrices and dense vectors on
      the GPU using CUDA.
    * Added a new iterative solver using CUDA.
    
    Change-Id: I4f3c5560eede502c1c1632f20869e9174c607fb7

--- internal/ceres/types.cc
+++ internal/ceres/types.cc
@@ -106,6 +106,7 @@ const char* SparseLinearAlgebraLibraryTypeToString(
     CASESTR(SUITE_SPARSE);
     CASESTR(EIGEN_SPARSE);
     CASESTR(ACCELERATE_SPARSE);
+    CASESTR(CUDA_SPARSE);
     CASESTR(NO_SPARSE);
     default:
       return "UNKNOWN";
@@ -118,6 +119,7 @@ bool StringToSparseLinearAlgebraLibraryType(
   STRENUM(SUITE_SPARSE);
   STRENUM(EIGEN_SPARSE);
   STRENUM(ACCELERATE_SPARSE);
+  STRENUM(CUDA_SPARSE);
   STRENUM(NO_SPARSE);
   return false;
 }
@@ -415,6 +417,14 @@ bool IsSparseLinearAlgebraLibraryTypeAvailable(
     return true;
 #else
     return false;
+#endif
+  }
+
+  if (type == CUDA_SPARSE) {
+#ifdef CERES_NO_CUDA
+    return false;
+#else
+    return true;
 #endif
   }
 
